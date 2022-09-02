387709cd0e88b2db007428379080290cff9212ac
Eugene Hauptmann <eugenehp@users.noreply.github.com>
2022-09-02 17:27:12 -0400

    added dynamic file

---
+++ apps/example-blog/src/components/header.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    
+    export let hello:string;
+
+    onMount(() => {
+        console.log(`the header component has mounted with hello=${hello}`);
+    });
+</script>
+  
+<style>
+    h3 {
+        color: green;
+    }
+</style>
+
+<h3>header component: {hello}</h3>
---
+++ apps/example-blog/src/routes/dates/dynamic.ts
@@ -0,0 +1,5 @@
+import Header from "../../components/header.svelte";
+
+const target = document.createElement('div')
+const header = new Header({target, hello: 'san francisco'})
+document.insertBefore(document.body.firstChild)

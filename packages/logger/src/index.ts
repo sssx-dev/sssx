83c8933d47ec5b4f8404c623a8bcae50ee1039e1
Eugene Hauptmann <eugenehp@users.noreply.github.com>
2022-09-21 10:53:31 -0400

    🚧 (@sssx/logger)

--- packages/logger/src/index.ts
+++ packages/logger/src/index.ts
@@ -1,11 +1,20 @@
 import colors from 'ansi-colors';
 
+export enum LogLevel {
+  VERBOSE = 'VERBOSE',
+  INFO = 'INFO',
+  DEBUG = 'DEBUG',
+  WARN = 'WARN',
+  ERROR = 'ERROR'
+}
+
 class Logger {
   private static instance?: Logger;
   private prefix = colors.blue('SSSX:');
+  private _level: LogLevel;
 
   constructor() {
-    //
+    this._level = LogLevel.INFO;
   }
 
   static getInstance() {
@@ -15,8 +24,30 @@ class Logger {
     return Logger.instance;
   }
 
-  log = (message?: unknown, ...optionalParams: unknown[]) => {
-    console.log(this.prefix, message, ...optionalParams);
+  set level(l: LogLevel) {
+    this._level = l;
+  }
+
+  get level() {
+    return this._level;
+  }
+
+  log = (...data: unknown[]) => {
+    console.log(this.prefix, ...data);
+  };
+
+  info = this.log;
+
+  verbose = (...data: unknown[]) => {
+    console.log(this.prefix, ...data);
+  };
+
+  warn = (...data: unknown[]) => {
+    console.warn(this.prefix, ...data);
+  };
+
+  error = (...data: unknown[]) => {
+    console.error(this.prefix, ...data);
   };
 }
 

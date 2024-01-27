84c092083b6a570938fd45acbdef0c54e98b9170
Eugene Hauptmann <eugene@reactivelions.com>
2024-01-27 14:28:30 -0500

    initial commit

---
+++ TODO.md
@@ -0,0 +1,32 @@
+## Major steps
+
+- [ ] Connect to the routing API section
+- [ ] Create server that requests URL and renders it using Svelte templates and this build system
+- [ ] add option to upload assets online
+
+## Good to have
+
+- [ ] generate sitemap
+- [ ] generate robots.txt
+- [ ] Simple file system that looks obvious (i.e. SvelteKit)
+
+## TODO
+
+- [ ] Research Astro, Svelte, Next layouts
+- [ ] Research esbuild plugins with virtual file system, and custom inputs
+- [ ] Develop separate systems for dev and build (vite style preview) to run separate modes
+
+## General list
+
+- [ ] Add javascript uglifier (compress too)
+- [ ] Enable assets (image) hashes
+- [ ] Clearly separate public assets in `public` folder and inside `src/assets`
+- [ ] optimize for lighthouse performance: First Contentful Paint (FCP)
+- [ ] improve hashing of names
+- [ ] decide how to serve CSS
+- [ ] add tailwind support
+- [ ] get the ssr -> html rendering as a standalone function
+- [ ] add logging and error reporting
+- [ ] abort if anything fails
+- [ ] figure out how to import images naturally `import logo from './assets/svelte.svg'`
+- [ ] add pretty errors for the `dev` mode – https://www.npmjs.com/package/youch
---
+++ build.ts
@@ -0,0 +1,158 @@
+import fs from "fs";
+import * as fsExtra from "fs-extra";
+import esbuild, { type BuildOptions, type Plugin } from "esbuild";
+import sveltePlugin from "esbuild-svelte";
+import sveltePreprocess from "svelte-preprocess";
+import inlineImage from "esbuild-plugin-inline-image";
+import copyPlugin from "esbuild-plugin-copy";
+import htmlPlugin from "@chialab/esbuild-plugin-html";
+import pretty from "pretty";
+
+const outdir = `./dev`;
+const finalOutdir = `./dist`;
+const enableSourcemap = false;
+const logLevel = `info`;
+const sourcemap = "inline";
+const prettify = false;
+
+if (!fs.existsSync(outdir)) {
+  fs.mkdirSync(outdir);
+} else {
+  fsExtra.emptyDirSync(outdir);
+}
+
+if (fs.existsSync(finalOutdir)) {
+  fsExtra.emptyDirSync(finalOutdir);
+}
+
+const copyPlugins = [
+  copyPlugin({
+    resolveFrom: "cwd",
+    assets: {
+      from: ["./public/*"],
+      to: [outdir],
+    },
+  }),
+  // not the best way to deal with the images
+  copyPlugin({
+    resolveFrom: "cwd",
+    assets: {
+      from: ["./src/**/*.svg"],
+      to: [outdir],
+    },
+  }),
+];
+
+let common: BuildOptions = {
+  entryPoints: [`./src/main.ts`],
+  bundle: true,
+  // outdir,
+  mainFields: ["svelte", "browser", "module", "main"],
+  conditions: ["svelte", "browser"],
+  logLevel,
+  minify: false, //so the resulting code is easier to understand
+  splitting: true,
+  write: true,
+  format: `esm`,
+};
+
+const ssrFile = `${outdir}/ssr.js`;
+
+// server
+await esbuild
+  .build({
+    ...common,
+    entryPoints: [`./src/App.svelte`],
+    //
+    outfile: ssrFile,
+    splitting: false,
+    //
+    plugins: [
+      sveltePlugin({
+        preprocess: sveltePreprocess(),
+        compilerOptions: {
+          generate: "ssr",
+          css: "injected",
+          hydratable: true,
+          // enableSourcemap,
+        },
+      }),
+    ],
+  })
+  .catch((reason) => {
+    console.warn(`Errors: `, reason);
+    process.exit(1);
+  });
+
+////////////////////////////////////////////////////////////
+
+const App = (await import(ssrFile)).default;
+const output = App.render();
+const title = `Custom Title Code`;
+
+const html = `
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>${title}</title>
+
+    <!-- <link rel="preload" href="./main.css" as="style" /> -->
+    <link rel="preload" href="./main.js" as="script" />
+  
+    <link rel="stylesheet" href="./main.css">
+    <!-- <style>${output.css.code}</style> -->
+  </head>
+  <body>
+    <div id="app">${output.html}</div>
+    <script type="module" src="./main.js"></script>
+  </body>
+</html>
+`;
+
+fs.writeFileSync(`${outdir}/main.css`, output.css.code, "utf8");
+if (prettify) {
+  fs.writeFileSync(`${outdir}/index.html`, pretty(html), "utf8");
+} else {
+  fs.writeFileSync(`${outdir}/index.html`, html, "utf8");
+}
+
+////////////////////////////////////////////////////////////
+
+// client
+await esbuild
+  .build({
+    ...common,
+    // sourcemap,
+    outfile: `${outdir}/main.js`,
+    splitting: false,
+    minify: true,
+    plugins: [
+      ...copyPlugins,
+      sveltePlugin({
+        preprocess: sveltePreprocess(),
+        compilerOptions: {
+          // css: 'none',
+          hydratable: true,
+        },
+      }),
+    ],
+  })
+  .catch((reason) => {
+    console.warn(`Errors: `, reason);
+    process.exit(1);
+  });
+
+// get inspired https://chialab.github.io/rna/guide/
+// await esbuild.build({
+//   entryPoints: [`${outdir}/index.html`],
+//   outdir: finalOutdir,
+//   // assetNames: "assets/[name]-[hash]", // breaks images for now
+//   assetNames: "assets/[name]",
+//   chunkNames: "[ext]/[name]-[hash]",
+//   plugins: [htmlPlugin()],
+// });
+
+fs.cpSync(outdir, finalOutdir, { recursive: true });

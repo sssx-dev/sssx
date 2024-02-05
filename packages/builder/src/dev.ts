e0f2c23a044680e1ebdea1f6839f583e7a7fd6bf
Eugene Hauptmann <eugene@reactivelions.com>
2024-02-05 15:56:54 -0500

    added notes

--- packages/builder/src/dev.ts
+++ packages/builder/src/dev.ts
@@ -40,6 +40,10 @@ await generateSSR(`${cwd}/src/App.svelte`, ssrFile, common);
 await renderSSR(ssrFile, outdir);
 await generateClient(outdir, common, {}, copyPlugins);
 
+// TODO: generate main.ts on the fly
+// TODO: replace App.svelte based on the route pages/path, and later content
+// TODO: replace import images with the string URLs and copy files to a destination using esbuild plugin
+
 app.use(express.static(outdir));
 const port = process.env.PORT ? parseInt(process.env.PORT) : 8080;
 const host = "127.0.0.1";

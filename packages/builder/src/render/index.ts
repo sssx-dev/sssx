5b8321cce862940cac1a3e4e1e557a17ac6623fe
Eugene Hauptmann <eugene@reactivelions.com>
2024-02-07 18:39:51 -0500

    wip

--- packages/builder/src/render/index.ts
+++ packages/builder/src/render/index.ts
@@ -1,3 +1,4 @@
+import { globby } from "globby";
 import { resolveImages } from "../plugins/resolveImages";
 import { getRoute } from "../utils/getRoute";
 import { rimraf } from "../utils/rimraf";
@@ -5,6 +6,15 @@ import { getCommonBuildOptions } from "../utils/settings";
 import { generateClient } from "./generateClient";
 import { generateSSR } from "./generateSSR";
 import { renderSSR } from "./renderSSR";
+
+// TODO: turn this into a proper matching logic
+// https://kit.svelte.dev/docs/advanced-routing
+const routeToFileSystem = async (srcDir: string) => {
+  const list = (await globby(`${srcDir}/**/+page.svelte`)).map((path) =>
+    path.replace(srcDir, "")
+  );
+  console.log("routeToFileSystem", list);
+};
 
 export const buildRoute = async (
   url: string,
@@ -14,6 +24,8 @@ export const buildRoute = async (
 ) => {
   const route = getRoute(url);
   const ssrFile = `${outdir}/ssr.js`;
+
+  await routeToFileSystem(`${base}pages/`);
 
   console.log({ route });
 

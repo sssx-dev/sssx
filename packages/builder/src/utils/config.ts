eecd798b9b11268c429f6d43ce413f584050fa85
Eugene Hauptmann <eugene@reactivelions.com>
2024-02-09 00:15:36 -0500

    added site to config

--- packages/builder/src/utils/config.ts
+++ packages/builder/src/utils/config.ts
@@ -2,10 +2,13 @@
 // https://nextjs.org/docs/app/api-reference/next-config-js
 // https://kit.svelte.dev/docs/configuration
 // https://elderguide.com/tech/elderjs/#config-elderconfigjs
+// https://docs.astro.build/en/reference/configuration-reference/
 export type Config = {
   title?: string;
   assets: string;
   outDir: string;
+  site?: string;
+  // baseDir?: string; // a base dir where website will be hosted
 };
 
 const defaultConfig: Config = {
--- packages/example/sssx.config.ts
+++ packages/example/sssx.config.ts
@@ -1,6 +1,7 @@
 const sssxConfig: any = {
   title: "Sssx Example Title",
   assets: "public",
+  site: "https://example.com/",
 };
 
 export default sssxConfig;

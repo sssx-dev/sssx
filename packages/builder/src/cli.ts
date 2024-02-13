fd14f1da9fe9c31497c561891a5329d70cb910ed
Eugene Hauptmann <eugene@reactivelions.com>
2024-02-12 22:32:05 -0500

    added colors to cli

---
+++ packages/builder/src/cli.ts
@@ -0,0 +1,42 @@
+#!/usr/bin/env npx tsx
+
+import chalk from "chalk";
+
+const [cmd, ...params] = process.argv.slice(2);
+
+if (!["dev", "build", "cluster", "clean", "urls"].includes(cmd)) {
+  console.log(`Usage:`);
+  console.log(`\t$ sssx <input>`);
+  console.log("");
+  console.log(`\t${chalk.bgGreen("dev")} – run in development mode`);
+  console.log(
+    `\t\t${chalk.bgGreen(
+      "dev open"
+    )} – if you want to open url in browser automatically`
+  );
+  console.log("");
+  console.log(
+    `\t${chalk.bgBlue("build")} – run in production mode (and build all)`
+  );
+  console.log(
+    `\t\t${chalk.bgBlue(
+      "build <url>"
+    )} – run in production mode and build only single <url>`
+  );
+  console.log("");
+  console.log(
+    `\t${chalk.bgMagenta(
+      "cluster"
+    )} – run in cluster production mode and use all CPU cores (and build all)`
+  );
+  console.log("");
+  console.log(
+    `\t${chalk.bgCyan("urls prefix")} – prints urls with a given prefix`
+  );
+  console.log("");
+  console.log(`\t${chalk.bgWhite("clean")} – cleans existing files`);
+} else {
+  await import(`./commands/${cmd}.ts`);
+}
+
+export {};

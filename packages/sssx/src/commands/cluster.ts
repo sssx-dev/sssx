564e85a7ae9fa99a6272cbb5e5af168f418f3a1d
Eugene Hauptmann <eugene@reactivelions.com>
2024-02-16 04:42:36 -0500

    added important note that costed me 6 hours

--- packages/sssx/src/commands/cluster.ts
+++ packages/sssx/src/commands/cluster.ts
@@ -66,6 +66,7 @@ type Message = {
 };
 
 const workerPath = import.meta.resolve("./worker.ts").replace("file://", "");
+// this was crucial to have to be able to run ts-node, because in tsx this would not run without it at all!
 const execArgv = ["--require", "ts-node/register", "--import", 'data:text/javascript,import { register } from "node:module"; import { pathToFileURL } from "node:url"; register("ts-node/esm", pathToFileURL("./"));', "--trace-warnings"]
 // Create workers
 for (var i = 0; i < numCPUs; i++) {

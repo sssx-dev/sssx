31ffc3f6fde7d6dfea2215b5d8318050575c9e93
Eugene Hauptmann <eugene@reactivelions.com>
2024-02-15 21:12:16 -0500

    clean

--- packages/sssx/src/indexes/loadExistingModule.ts
+++ packages/sssx/src/indexes/loadExistingModule.ts
@@ -4,8 +4,6 @@ import { Module } from "./types.ts";
 export const loadExistingModule = async (fullpath: string) => {
   if (fs.existsSync(fullpath)) {
     const rand = Math.random().toString().slice(2);
-    // hack: to prevent module import caching, we add a random number as a url parameter
-    // revisit this for deno
     const module: Module = await import(`${fullpath}?${rand}`);
     return module;
   }
